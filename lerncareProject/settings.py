04559ca7d214ac24c3a509db2ca231e1dc9c5fcc
João Rodrigues <johnnyrod4569@gmail.com>
2023-11-14 11:52:52 +0100

    fix: Attempted fix of deployment bug by re-difing databases and set psycopg2-binary back to psycopg2 in requirements.txt

--- lerncareProject/settings.py
+++ lerncareProject/settings.py
@@ -14,8 +14,10 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.isfile("env.py"):
+try:
     import env
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,19 +85,16 @@ WSGI_APPLICATION = 'lerncareProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    print("Postgres URL not found, using sqlite instead")
+}
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+if "DATABASE_URL" in os.environ:
+    DATABASES['default'] = dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+
 
 
 # Password validation
