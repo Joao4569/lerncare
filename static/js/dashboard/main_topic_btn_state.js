f3feadb5647d7988f894e49679c7dcb1045c3dbd
João Rodrigues <johnnyrod4569@gmail.com>
2023-12-05 18:15:26 +0100

    feat: Add functionality to store main topic and aspect selected by user in session and started working on topic to aspect logic for new models

--- static/js/dashboard/main_topic_btn_state.js
+++ static/js/dashboard/main_topic_btn_state.js
@@ -1,3 +1,6 @@
+// Get all the main topic buttons
+let main_topic_btns = document.getElementsByClassName("main-topic-btn");
+
 //  Triggered by: dashboard.html
 mainTopicBtnState();
 
@@ -11,11 +14,8 @@ document.getElementById("next-button").addEventListener("click", function() {
     clearMainTopicBtnState()
 });
 
-function mainTopicBtnState() {
 // Function to change the state of the main topic buttons with user input
-    
-    // Get all the main topic buttons
-    let main_topic_btns = document.getElementsByClassName("main-topic-btn");
+function mainTopicBtnState() {
 
     // Loop through the main topic buttons
     for (let btn of main_topic_btns) {
@@ -27,12 +27,19 @@ function mainTopicBtnState() {
 
             // Add the class to the clicked button
             this.className += " main-topic-btn-selected";
+
+            // TESTING
+            aspectTileBtnState();
+            // TESTING
+
+            // Collect main topic name in session storage - js/dashboard/custom_session_info.js
+            currentMainTopicSessionExtraction()
         });
     }
 }
 
-function clearMainTopicBtnState() {
 // Function to clear the state of the main topic buttons
+function clearMainTopicBtnState() {
 
     // Get the currently selected button
     let current = document.getElementsByClassName("main-topic-btn-selected");
@@ -43,3 +50,23 @@ function clearMainTopicBtnState() {
     }
 
 }
+
+// TESTING
+
+// Function to able aspect tile buttons upon main topic selection
+function aspectTileBtnState() {
+
+    // Get the currently selected button
+    let currently_disabled_buttons = document.getElementsByClassName("disabled");
+    console.log(currently_disabled_buttons);
+    // If there is a currently selected button, remove the class from it
+    if (currently_disabled_buttons.length > 0) {
+        current[0].className = current[0].className.replace(" disabled", "");
+    }
+    console.log('success');
+
+    
+
+}
+
+// TESTING
