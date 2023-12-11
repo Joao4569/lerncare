9c1d1b20230a2f11eab8854ece66ff8ed2126940
João Rodrigues <johnnyrod4569@gmail.com>
2023-12-11 11:59:58 +0100

    feat: Finish logic to pass the user selected topic and aspect to the url for the aspect template logic needed to display the related info from the AspectContent model

--- static/js/dashboard/custom_session_info.js
+++ static/js/dashboard/custom_session_info.js
@@ -1,21 +1,21 @@
-// This function is called when the main topic button is clicked and sets the main topic name in a session variable
+// This function is called when a main topic button is clicked and sets the main topic name in a session variable
 function currentMainTopicSessionExtraction() {
     let mainTopic = document.getElementsByClassName("main-topic-btn-selected");
-    let mainTopicName = mainTopic[0].innerText;
+    let mainTopicName = mainTopic[0].id;
     sessionStorage.setItem('mainTopicName', mainTopicName);
 }
 
-// This function is called when the aspect button is clicked and sets the aspect name in a session variable
-// document.getElementById('vignette-btn').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     let userSelectedAspect = 'vignette';
-//     sessionStorage.setItem('userSelectedAspect', userSelectedAspect);
-// });
+// This function is called when a aspect button is clicked and sets the aspect name in a session variable
+document.getElementById('vignette').addEventListener('click', function(event) {
+    event.preventDefault();
+    let userSelectedAspect = this.id;
+    sessionStorage.setItem('userSelectedAspect', userSelectedAspect);
+});
 
-// When the Vignette aspect button link is clicked
+// This will allocate the user selected aspect and maintopic names to the url
 document.querySelector('.dropdown-item').addEventListener('click', function(event) {
     event.preventDefault();
     let mainTopic = sessionStorage.getItem('mainTopicName');
-    // let aspectTile = sessionStorage.getItem('userSelectedAspect');
-    window.location.href = this.href + mainTopic + '/';
+    let aspectTile = sessionStorage.getItem('userSelectedAspect');
+    window.location.href = this.href + aspectTile + '/' + mainTopic;
 });
